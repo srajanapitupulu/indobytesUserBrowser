b18810af5f43ae080dd5739bd0340fcbd0f1c0b8
srajanapitupulu <srajanapitupulu@gmail.com>
2024-08-25 12:54:44 +0700

    [Initial Commit] Project configuration and initiation for Indobytes Solution take-home project

---
+++ indobytesUserBrowser/indobytesUserBrowserApp.swift
@@ -0,0 +1,20 @@
+//
+//  indobytesUserBrowserApp.swift
+//  indobytesUserBrowser
+//
+//  Created by bm yanti on 25/08/24.
+//
+
+import SwiftUI
+
+@main
+struct indobytesUserBrowserApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ indobytesUserBrowserUITests/indobytesUserBrowserUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  indobytesUserBrowserUITestsLaunchTests.swift
+//  indobytesUserBrowserUITests
+//
+//  Created by bm yanti on 25/08/24.
+//
+
+import XCTest
+
+final class indobytesUserBrowserUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
